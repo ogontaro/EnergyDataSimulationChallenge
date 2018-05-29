c476c4be927a5919add796ee56b8de8365f164a4
ryotaro-uchigane <ryotaro.uchigane@theport.jp>
2018-05-29 20:06:55 +0900

    add seed

--- challenge3/webapp/ogontaro/db/seeds.rb
+++ challenge3/webapp/ogontaro/db/seeds.rb
@@ -1,7 +1,40 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+p 'seed creating...'
+
+p 'house data creating...'
+CSV.foreach('./db/fixture/house_data.csv', headers: true) do |row|
+  House.find_or_create_by(id: row['id'],
+                          first_name: row['Firstname'],
+                          last_name: row['Lastname'],
+                          city: row['City'],
+                          num_of_people: row['num_of_people'],
+                          has_child: row['has_child'])
+end
+p 'house data created'
+
+p 'energy data creating...'
+CSV.foreach('./db/fixture/dataset_50.csv', headers: true) do |row|
+  Energy.find_or_create_by(id: row['id'],
+                           label: row['Label'],
+                           house_id: row['House'],
+                           year: row['Year'],
+                           month: row['Month'],
+                           date: Date.new(row['Year'].to_i, row['Month'].to_i, 1),
+                           temperature: row['Temperature'],
+                           daylight: row['Daylight'],
+                           energy_production: row['EnergyProduction'])
+end
+p 'energy data created'
+
+p 'seed created'
